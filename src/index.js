15fe6cd6676f6df5f8c28f6b587d5c00f2483d7d
MonicaEsparza26 <84533659+MonicaEsparza26@users.noreply.github.com>
2021-07-07 17:42:10 -0500

    users

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('hello world, im Moon');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const list = [];
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/users', (req,res) =>  res.json( list ));
+
+app.post('/users', (req,res) => {
+    list.push({id:0});
+    res.json(user);
+});
+
+app.get('/user/:id', (req,res) => {
+    const user = list.find(usr => usr.id === req.params.id);
+    res.json(user)
+});
+
+app.listen(port, () => console.log(`Server on port ${port}!`))
